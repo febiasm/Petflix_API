396e91ab4b64b19958b84f69016c3fc7c465317b
febiasm <trevorfebias@gmail.com>
2025-05-07 00:52:40 +0300

    added CROS

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// --- Add CORS headers middleware ---
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+ 
 app.get('/organizations', (req, res) => {
   fs.readFile('./data.json', 'utf8', (err, data) => {
     if (err) return res.status(500).send('Error reading data.');
